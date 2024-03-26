311e36c34f9be0493aa5e15ed85d304b701c359e
anixnoyal <anil.noc@gmail.com>
2024-03-26 13:09:35 +0530

    Update setup_litmus_portal.sh

--- setup_litmus_portal.sh
+++ setup_litmus_portal.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 # Install Helm
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 
@@ -15,10 +13,9 @@ kubectl create namespace litmus
 # Install Litmus ChaosCenter
 helm search repo litmuschaos
 helm install chaos litmuschaos/litmus --namespace litmus --debug
-echo "Litmus Chaos Portal installation complete."
 
 kubectl get svc -n litmus
-kubectl port-forward svc/litmusportal-frontend-service -n litmus 9091:9091
+kubectl port-forward service/chaos-litmus-frontend-service  -n litmus 9091:9091
 kubectl get all -n litmus
 
 ## uninstall helm
